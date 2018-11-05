b88cfb8efbbab6669ebcac4467b33769ca22103a
git <marvin.meyer1@aol.de>
2018-11-05 22:33:40 +0100

    redesign

--- calc.js
+++ calc.js
@@ -2,37 +2,42 @@ document.addEventListener('DOMContentLoaded', function (event) {
     document.getElementById('calc').addEventListener('click', function () {
         let first = Number(document.getElementById('first').value);
         let second = Number(document.getElementById('second').value);
-        let symbol = document.getElementById('symbol').value;
-        let result = 0;
-        let history_element = document.createElement('p');
+        let symbols = document.getElementsByName('operation');
 
-        switch (symbol) {
-            case 'multiplication':
-                result = first * second;
-                document.getElementById('result').value = result;
-                history_element.innerHTML = 'Rechnung: ' + first + ' * ' + second + ' = ' + result;
-                document.getElementById('history').appendChild(history_element);
-                break;
-            case 'division':
-                result = first / second;
-                document.getElementById('result').value = result;
-                history_element.innerHTML = 'Rechnung: ' + first + ' / ' + second + ' = ' + result;
-                document.getElementById('history').appendChild(history_element);
-                break;
-            case 'addition':
-                result = first + second;
-                document.getElementById('result').value = result;
-                history_element.innerHTML = 'Rechnung: ' + first + ' + ' + second + ' = ' + result;
-                document.getElementById('history').appendChild(history_element);
-                break;
-            case 'subtraction':
-                result = first - second;
-                document.getElementById('result').value = result;
-                history_element.innerHTML = 'Rechnung: ' + first + ' - ' + second + ' = ' + result;
-                document.getElementById('history').appendChild(history_element);
-                break;
-            default:
-                break;
+        let history = document.createElement('p');
+        let result = 0;
+
+        for (let i = 0, length = symbols.length; i < length; i++) {
+            if (symbols[i].checked) {
+                switch (symbols[i].value) {
+                    case 'multiply':
+                        result = first * second;
+                        document.getElementById('result').value = result;
+                        history.innerHTML = 'Rechnung: ' + first + ' * ' + second + ' = ' + result;
+                        document.getElementById('history').appendChild(history);
+                        break;
+                    case 'divide':
+                        result = first / second;
+                        document.getElementById('result').value = result;
+                        history.innerHTML = 'Rechnung: ' + first + ' / ' + second + ' = ' + result;
+                        document.getElementById('history').appendChild(history);
+                        break;
+                    case 'add':
+                        result = first + second;
+                        document.getElementById('result').value = result;
+                        history.innerHTML = 'Rechnung: ' + first + ' + ' + second + ' = ' + result;
+                        document.getElementById('history').appendChild(history);
+                        break;
+                    case 'subtract':
+                        result = first - second;
+                        document.getElementById('result').value = result;
+                        history.innerHTML = 'Rechnung: ' + first + ' - ' + second + ' = ' + result;
+                        document.getElementById('history').appendChild(history);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     });
 });
