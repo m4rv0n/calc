cc293e36300963b8de8a0b7a816a6922a930f874
git <marvin.meyer@mailbox.org>
2018-11-06 00:00:53 +0100

    align values when grouped

--- calc.js
+++ calc.js
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     document.getElementById('calc').addEventListener('click', function () {
-        let first = Number(document.getElementById('first').value);
-        let second = Number(document.getElementById('second').value);
+        let first   = Number(document.getElementById('first').value);
+        let second  = Number(document.getElementById('second').value);
         let symbols = document.getElementsByName('operation');
 
         let history = document.createElement('p');
-        let result = 0;
+        let result  = 0;
 
         for (let i = 0, length = symbols.length; i < length; i++) {
             if (symbols[i].checked) {
                 switch (symbols[i].value) {
                     case 'multiply':
-                        result = first * second;
+                        result                                  = first * second;
                         document.getElementById('result').value = result;
-                        history.innerHTML = 'calculation: ' + first + ' * ' + second + ' = ' + result;
+                        history.innerHTML                       = 'calculation: ' + first + ' * ' + second + ' = ' + result;
                         document.getElementById('history').appendChild(history);
                         break;
                     case 'divide':
-                        result = first / second;
+                        result                                  = first / second;
                         document.getElementById('result').value = result;
-                        history.innerHTML = 'calculation: ' + first + ' / ' + second + ' = ' + result;
+                        history.innerHTML                       = 'calculation: ' + first + ' / ' + second + ' = ' + result;
                         document.getElementById('history').appendChild(history);
                         break;
                     case 'add':
-                        result = first + second;
+                        result                                  = first + second;
                         document.getElementById('result').value = result;
-                        history.innerHTML = 'calculation: ' + first + ' + ' + second + ' = ' + result;
+                        history.innerHTML                       = 'calculation: ' + first + ' + ' + second + ' = ' + result;
                         document.getElementById('history').appendChild(history);
                         break;
                     case 'subtract':
-                        result = first - second;
+                        result                                  = first - second;
                         document.getElementById('result').value = result;
-                        history.innerHTML = 'calculation: ' + first + ' - ' + second + ' = ' + result;
+                        history.innerHTML                       = 'calculation: ' + first + ' - ' + second + ' = ' + result;
                         document.getElementById('history').appendChild(history);
                         break;
                     default:
